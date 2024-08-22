7e88a995a73201eb41189a4355ede17352a5aa8b
ZainJ5 <154011623+ZainJ5@users.noreply.github.com>
2024-08-22 09:48:49 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -178,7 +178,7 @@ async function confirmOrder(email, firstName, lastName, address, apartmentSuite,
       <tbody>
         ${ordered && ordered.length > 0 ? ordered.map(item => `
           <tr>
-            <td>${item.shoeName}</td>
+            <td><img src="https://backendnike.onrender.com/public/images/${item.shoeName}" alt="${item.shoeName}"></td>
             <td><img src="http://localhost:5000/images/${item.shoeImage}" alt="${item.shoeName}" width="100"></td>
             <td>${item.shoeSize}</td>
             <td>$${item.shoePrice}</td>
